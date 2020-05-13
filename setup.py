e9223ed10e25169483c224e699684498362fffce
Lowe.li <1445620939@qq.com>
2020-05-13 14:08:52 +0800

    add all

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+setuptools.setup(name='deeplearn',
+      version='0.1',
+      description='clinical trial information retriver',
+      url='http://github.com/tongling/clinicaltrial',
+      author='lowe',
+      author_email='alilzg@aliyun.com',
+      license='MIT',
+      packages=setuptools.find_packages(),
+      zip_safe=False)
